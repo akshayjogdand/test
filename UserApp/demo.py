34c1f47e23771b4b5524b2ca3f68d1f23b48279c
akshay <akshayjogdand24@gmail.com>
2024-02-19 16:11:01 +0530

    Initial commit

---
+++ UserApp/demo.py
@@ -0,0 +1,12 @@
+list1 = ["ak","j","sh,","er"]
+list12 = [11,13,34,55]
+
+op = "".join([str(i) for i in list12 ])
+print(op)
+
+"""#result = [i for i in list1]
+result = "".join(list1)
+print(result)"""
+
+
+
